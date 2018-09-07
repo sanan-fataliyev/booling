3757eaf42966ba9007dbc709aa0898a6d79d4ed3
Sanan Fataliyev <sanan.fataliyev@gmail.com>
2018-09-07 17:31:36 +0400

    BoolTable.ValidateCellAddress method add

--- src/Booling/BoolTable.cs
+++ src/Booling/BoolTable.cs
@@ -10,17 +10,31 @@ namespace Booling
         private ulong _bits;
 
         private const int __RowsCount = 8;
-        private const int __BitsPerRow = 8; 
+        private const int __BitsPerRow = 8;
+
+
+        private void ValidateCellAddress(int row, int col)
+        {
+            if(row < 0 || row >= __RowsCount || col < 0 || col >= __BitsPerRow)
+                throw new IndexOutOfRangeException();
+        }
+
 
         public int Rank => 2;
         
 
         public bool this[int row, int col]
         {
-            get => (this._bits & (1UL << (row * __BitsPerRow + col))) > 0UL;
+            get
+            {
+                ValidateCellAddress(row, col);
+                return (this._bits & (1UL << (row * __BitsPerRow + col))) > 0UL;
+            }
 
             set
             {
+                ValidateCellAddress(row, col);
+
                 if (value)
                 {
                     this._bits |= 1UL << (row * __BitsPerRow + col);
@@ -34,7 +48,7 @@ namespace Booling
 
         public override string ToString()
         {
-            return Convert.ToString((long)this._bits, 2);
+            return Convert.ToString((long) this._bits, 2).PadLeft(64, '0');
         }
     }
 }
