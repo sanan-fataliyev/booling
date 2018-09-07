ef59ad3b0001ecd5511843549c51404538dd66e0
Sanan Fataliyev <sanan.fataliyev@gmail.com>
2018-09-07 12:17:17 +0400

    init commit

---
+++ src/Booling/BoolTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booling
+{
+    class BoolTable : IBoolTable
+    {
+        public bool this[int m, int n]
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ src/Booling/IBoolTable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booling
+{
+    interface IBoolTable /*IEnumerable<bool>, IEnumerable<bool[]>*/
+    {
+        bool this[int m, int n] { get; set; }
+    }
+}
