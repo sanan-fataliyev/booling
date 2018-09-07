a77cf54bab4bcae2bf54f463a68b796992b52404
Sanan Fataliyev <sanan.fataliyev@gmail.com>
2018-09-07 16:01:20 +0400

    InitializeAllCellsWithFalseInDefaultConstructorTest case added

--- src/Booling.Tests/BoolTableTest.cs
+++ src/Booling.Tests/BoolTableTest.cs
@@ -1,12 +1,27 @@
+using System;
+using Booling;
 using NUnit.Framework;
 
 namespace Tests
 {
+    [TestFixture]
     public class Tests
     {
         [SetUp]
         public void Setup()
         {
         }
+
+        [Test]
+        public void InitializeAllCellsWithFalseInDefaultConstructorTest()
+        {
+            BoolTable bt = new BoolTable();
+
+            for (int row = 0; row < Math.Sqrt(sizeof(ulong)); row++)
+                for (int col = 0; col < Math.Sqrt(sizeof(ulong)); col++)
+                    Assert.IsFalse(bt[row, col]);
+
+        }
+
     }
 }
